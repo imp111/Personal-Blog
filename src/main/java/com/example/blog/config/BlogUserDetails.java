e6a43d1e534569ec2f1bc76523bcdc0f1d651bca
imp111 <alexsandartenev@gmail.com>
2021-08-19 00:24:54 +0300

    bug fixed

--- src/main/java/com/example/blog/config/BlogUserDetails.java
+++ src/main/java/com/example/blog/config/BlogUserDetails.java
@@ -1,7 +1,22 @@
+package com.example.blog.config;
+
 import com.example.blog.entity.User;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+
 public class BlogUserDetails extends User implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
     @Override
     public boolean isAccountNonExpired()
     {
