10af1e97378337eea886b285c405a1bc3286899a
imp111 <alexsandartenev@gmail.com>
2021-08-26 14:38:29 +0300

    category creation functionality added

--- src/main/java/com/example/blog/controller/admin/CategoryController.java
+++ src/main/java/com/example/blog/controller/admin/CategoryController.java
@@ -1,12 +1,15 @@
 package com.example.blog.controller.admin;
 
+import com.example.blog.bindingModel.CategoryBindingModel;
 import com.example.blog.entity.Category;
 import com.example.blog.repository.ArticleRepository;
 import com.example.blog.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Comparator;
@@ -22,6 +25,7 @@ public class CategoryController {
     @Autowired
     private ArticleRepository articleRepository;
 
+    // List all categories
     @GetMapping("/")
     public String list(Model model)
     {
@@ -35,5 +39,30 @@ public class CategoryController {
         model.addAttribute("categories", categories);
 
         return "base-layout";
+    }
+
+    // Returns only creation form
+    @GetMapping("/create")
+    public String create(Model model)
+    {
+        model.addAttribute("view", "admin/category/create");
+
+        return "base-layout";
+    }
+
+    // Create new category
+    @PostMapping("/create")
+    public String createProcess(CategoryBindingModel categoryBindingModel)
+    {
+        if (StringUtils.isEmpty(categoryBindingModel.getName()))
+        {
+            return "redirect:/admin/categories/create"; // "redirect:/admin/category/create";
+        }
+
+        Category category = new Category(categoryBindingModel.getName());
+
+        this.categoryRepository.saveAndFlush(category);
+
+        return "redirect:/admin/categories/";
     }
 }
