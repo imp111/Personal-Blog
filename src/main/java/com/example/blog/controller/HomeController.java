10cd41439f5a179b4207ed0b81b33b63d8891989
imp111 <alexsandartenev@gmail.com>
2021-08-26 14:08:01 +0300

    access denied for normal users trying to go in /admin

--- src/main/java/com/example/blog/controller/HomeController.java
+++ src/main/java/com/example/blog/controller/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -21,6 +22,15 @@ public class HomeController {
 
         model.addAttribute("view", "home/index");
         model.addAttribute("articles", articles);
+
+        return "base-layout";
+    }
+
+    // Redirect to 403.html if we try to access /admin from user account
+    @RequestMapping("/error/403")
+    public String accessDenied(Model model)
+    {
+        model.addAttribute("view", "error/403");
 
         return "base-layout";
     }
