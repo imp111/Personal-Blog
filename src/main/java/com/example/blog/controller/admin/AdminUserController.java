4598d46a4e7b6df0e0a78be0b5d7d29451622480
imp111 <alexsandartenev@gmail.com>
2021-08-26 13:27:35 +0300

    edit profile role

--- src/main/java/com/example/blog/controller/admin/AdminUserController.java
+++ src/main/java/com/example/blog/controller/admin/AdminUserController.java
@@ -1,16 +1,24 @@
 package com.example.blog.controller.admin;
 
+import com.example.blog.bindingModel.UserEditBindingModel;
+import com.example.blog.entity.Role;
 import com.example.blog.entity.User;
 import com.example.blog.repository.ArticleRepository;
 import com.example.blog.repository.RoleRepository;
 import com.example.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/admin/users")
@@ -34,5 +42,62 @@ public class AdminUserController {
         model.addAttribute("view", "admin/user/list");
 
         return "base-layout";
+    }
+
+    // Edit users in the edit admin panel
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model)
+    {
+        if (!this.userRepository.existsById(id))
+        {
+            return "redirect:/admin/users/";
+        }
+
+        User user = this.userRepository.findById(id).orElse(null);
+        List<Role> roles = this.roleRepository.findAll();
+
+        model.addAttribute("user", user);
+        model.addAttribute("roles", roles);
+        model.addAttribute("view", "admin/user/edit");
+
+        return "base-layout";
+    }
+
+    //
+    @PostMapping("/edit/{id}")
+    public String editProcess(@PathVariable Integer id, UserEditBindingModel userBindingModel)
+    {
+        if (!this.userRepository.existsById(id))
+        {
+            return "redirect:/admin/users/";
+        }
+
+        User user = this.userRepository.findById(id).orElse(null);
+
+        if (!StringUtils.isEmpty(userBindingModel.getPassword()) && !StringUtils.isEmpty(userBindingModel.getConfirmPassword()))
+        {
+            if (userBindingModel.getPassword().equals(userBindingModel.getConfirmPassword()))
+            {
+                BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+                user.setPassword(bCryptPasswordEncoder.encode(userBindingModel.getPassword()));
+            }
+        }
+
+        user.setFullName(userBindingModel.getFullName());
+        user.setEmail(userBindingModel.getEmail());
+
+        Set<Role> roles = new HashSet<>();
+
+        for (Integer roleId : userBindingModel.getRoles())
+        {
+            roles.add(this.roleRepository.findById(roleId).orElse(null));
+        }
+
+        user.setRoles(roles);
+
+        this.userRepository.saveAndFlush(user);
+
+        return "redirect:/admin/users/";
     }
 }
