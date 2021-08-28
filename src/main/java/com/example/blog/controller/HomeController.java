9cb3e1490de0ab51b303c26ecd71d8ce08b08f20
imp111 <alexsandartenev@gmail.com>
2021-08-28 15:10:54 +0300

    categories fully added and working

--- src/main/java/com/example/blog/controller/HomeController.java
+++ src/main/java/com/example/blog/controller/HomeController.java
@@ -1,27 +1,36 @@
 package com.example.blog.controller;
 
 import com.example.blog.entity.Article;
+import com.example.blog.entity.Category;
 import com.example.blog.repository.ArticleRepository;
+import com.example.blog.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Set;
 
 @Controller
 public class HomeController {
+    //@Autowired
+    //private ArticleRepository articleRepository;
+
     @Autowired
-    private ArticleRepository articleRepository;
+    private CategoryRepository categoryRepository;
 
     @GetMapping("/")
     public String index(Model model)
     {
-        List<Article> articles = this.articleRepository.findAll();
+        //List<Article> articles = this.articleRepository.findAll();
+        List<Category> categories = this.categoryRepository.findAll();
 
         model.addAttribute("view", "home/index");
-        model.addAttribute("articles", articles);
+        //model.addAttribute("articles", articles);
+        model.addAttribute("categories", categories);
 
         return "base-layout";
     }
@@ -30,6 +39,24 @@ public class HomeController {
     public String accessDenied(Model model)
     {
         model.addAttribute("view", "error/403");
+
+        return "base-layout";
+    }
+
+    @GetMapping("/category/{id}")
+    public String listArticles(Model model, @PathVariable Integer id)
+    {
+        if (!this.categoryRepository.existsById(id))
+        {
+            return "redirect:/";
+        }
+
+        Category category = this.categoryRepository.findById(id).orElse(null);
+        Set<Article> articles = category.getArticles();
+
+        model.addAttribute("view", "home/list-articles");
+        model.addAttribute("articles", articles);
+        model.addAttribute("category", category);
 
         return "base-layout";
     }
